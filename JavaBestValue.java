6281fc8052d4e85069dc805e468d7ab8f27e0170
kurbycafe <43386485+kurbycafe@users.noreply.github.com>
2021-11-26 03:54:05 +0900

    added JavaSwitch file

---
+++ JavaBestValue.java
@@ -0,0 +1,10 @@
+public class JavaBestValue {
+    public static void main(String[] args) {
+        int num = 10000;
+        int key = 153;
+        double n =num /key;
+        System.out.println(n*key);
+
+
+    }
+}
